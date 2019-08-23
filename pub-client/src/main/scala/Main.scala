d970c1e6e9a5a69e13c089e53052b76a5c619476
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-08-23 13:52:49 -0700

    Send sequence of messages

--- pub-client/src/main/scala/Main.scala
+++ pub-client/src/main/scala/Main.scala
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.googlecloud.pubsub._
 import akka.stream.alpakka.googlecloud.pubsub.scaladsl.GooglePubSub
+import akka.stream.javadsl.RunnableGraph
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.collection.immutable.Seq
@@ -29,23 +30,33 @@ object Main extends App {
 
   val topic = "topic1"
 
-  val encodedMessage = new String(Base64.getEncoder.encode("Hello Google!".getBytes))
+//  val encodedMessage = new String(Base64.getEncoder.encode("Hello Google!".getBytes))
+  val message1 = new String(Base64.getEncoder.encode("Message1".getBytes))
+  val message2 = new String(Base64.getEncoder.encode("Message2".getBytes))
+  val message3 = new String(Base64.getEncoder.encode("Message3".getBytes))
 
-  val publishMessage =
-    PubSubMessage(encodedMessage)
-  val publishRequest = PublishRequest(Seq(publishMessage))
+//  val publishMessage =
+//    PubSubMessage(encodedMessage)
+
+  val publishMessages = Seq(PubSubMessage(message1), PubSubMessage(message2), PubSubMessage(message3))
+
+  val publishRequest = PublishRequest(publishMessages)
+
 
   val source: Source[PublishRequest, NotUsed] = Source.single(publishRequest)
 
   val publishFlow: Flow[PublishRequest, Seq[String], NotUsed] = GooglePubSub.publish(topic, config)
 
-
   val publishedMessageIds: Future[Seq[Seq[String]]] = source.via(publishFlow).runWith(Sink.seq)
+
+
 
   publishedMessageIds onComplete {
     case Success(data) => for (ids <- data) println(ids)
     case Failure(t) => println("error getting published messageId", t.getMessage)
   }
+
+
 
   try
     StdIn.readLine()
