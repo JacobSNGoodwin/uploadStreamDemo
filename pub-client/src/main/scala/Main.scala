d70066412f2a5e2184642e212c6ede2f2b1a2ed6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-08-28 21:19:15 -0700

    Remove unused imports

--- pub-client/src/main/scala/Main.scala
+++ pub-client/src/main/scala/Main.scala
@@ -6,22 +6,19 @@ import akka.event.Logging
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.googlecloud.pubsub._
 import akka.stream.alpakka.googlecloud.pubsub.scaladsl.GooglePubSub
-import akka.stream.Attributes
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.annotation.tailrec
 import scala.collection.immutable.Seq
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-import scala.io.StdIn
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success}
+
 
 object Main extends App {
-  implicit val system = ActorSystem()
+  implicit val system = ActorSystem("PublishClient")
   implicit val mat = ActorMaterializer()
 
-  implicit val log = Logging(system, "publishLogger")
+  implicit val log = Logging(system, "PublishLogger")
 
   val privateKey = system.settings.config.getString("gcConfig.key").replace("\\n", "\n")
 
@@ -51,14 +48,13 @@ object Main extends App {
     val source: Source[PublishRequest, NotUsed] = Source.single(publishRequest)
     val publishFlow: Flow[PublishRequest, Seq[String], NotUsed] = GooglePubSub.publish(topic, config)
     val publishedMessageIds: Future[Seq[Seq[String]]] = source.via(publishFlow).runWith(Sink.seq)
-    
+
     try {
       val result = Await.result(publishedMessageIds, 3 second)
       println(s"Message published with the following id: ${result}")
     } catch {
       case t: Throwable => println(s"Error publishing message: ${t.getMessage}")
     }
-
 
     val rePrompt = io.StdIn.readLine("Would you like to publish another message? (y to continue, other to exit) > ")
     rePrompt match {
