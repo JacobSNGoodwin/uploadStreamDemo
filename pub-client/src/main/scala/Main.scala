dfc52882d5a5eba61952dc96c3b8e49a5600249b
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-02 10:59:02 -0700

    Use an outer loop to create topic Flow

--- pub-client/src/main/scala/Main.scala
+++ pub-client/src/main/scala/Main.scala
@@ -23,10 +23,8 @@ object Main extends App {
   implicit val log = Logging(system, "PublishLogger")
 
   val privateKey = system.settings.config.getString("gcConfig.key").replace("\\n", "\n")
-
   val clientEmail = "uploadstreamdemo@uploadstream.iam.gserviceaccount.com"
   val projectId = "uploadstream"
-
   val config = PubSubConfig(projectId, clientEmail, privateKey)
 
   println(
@@ -35,53 +33,72 @@ object Main extends App {
       |""".stripMargin)
 
 
-  val gcFlow: Flow[(String, String), PublishRequest, NotUsed] = Flow[(String, String)]
-    .map(messageData => {
-      PublishRequest(Seq(
-        PubSubMessage(new String(Base64.getEncoder.encode(messageData._1.getBytes))))
-      )
-    })
-
-
-  val bufferSize = 10
-  val elementsToProcess = 5
-
-  // newSource is a Source[PublishRequest, NotUsed]
-  val (queue, newSource) = Source
-    .queue[(String, String)](bufferSize, OverflowStrategy.backpressure)
-    .via(gcFlow)
-    .preMaterialize()
-
-
-  val result = newSource.runForeach(println)
-
-//  result onComplete {
-//    case Success(ids) => println(ids)
-//    case Failure(exception) => log.error("Could not process message: " + exception.getMessage)
-//  }
-
-
-  @tailrec
-  def promptLoop(): Unit = {
+  def topicLoop(): Unit = {
     val topic = io.StdIn.readLine("Enter a Topic > ")
-    val message = io.StdIn.readLine("Enter a Message > ")
 
-    queue.offer((message, topic)).map {
-//      case QueueOfferResult.Enqueued    => println(s"enqueued message: $message, topic: $topic")
-      case QueueOfferResult.Dropped     => println(s"dropped message: $message, topic: $topic")
-      case QueueOfferResult.Failure(ex) => println(s"Offer failed ${ex.getMessage}")
-      case QueueOfferResult.QueueClosed => println("Source Queue closed")
+    // Create flow and queue based on entered topic
+      val gcFlow: Flow[String, Seq[String], NotUsed] = Flow[String]
+        .map(messageData => {
+          PublishRequest(Seq(
+            PubSubMessage(new String(Base64.getEncoder.encode(messageData.getBytes))))
+          )
+        })
+        .via(GooglePubSub.publish(topic, config))
+
+
+      val bufferSize = 10
+      val elementsToProcess = 5
+
+      // newSource is a Source[PublishRequest, NotUsed]
+      val (queue, newSource) = Source
+        .queue[String](bufferSize, OverflowStrategy.backpressure)
+        .via(gcFlow)
+        .preMaterialize()
+
+
+      newSource.runForeach(println)
+
+//      result onComplete {
+//        case Success(ids) => println(ids)
+//        case Failure(exception) => log.error("Could not process message: " + exception.getMessage)
+//      }
+
+      messageLoop()
+
+    @tailrec
+    def messageLoop(): Unit = {
+      println(s"A message will be created and sent to '${topic}' based on the groupId and deviceId entered below.")
+      val groupId = io.StdIn.readLine("Please provide the groupId  > ")
+      val deviceId = io.StdIn.readLine("Please provide the deviceId > ")
+
+      val message = s"$groupId-$deviceId"
+
+      queue.offer(message).map {
+        case QueueOfferResult.Enqueued    => println(s"enqueued message: $message, topic: $topic")
+        case QueueOfferResult.Dropped     => println(s"dropped message: $message")
+        case QueueOfferResult.Failure(ex) => println(s"Offer failed ${ex.getMessage}")
+        case QueueOfferResult.QueueClosed => println("Source Queue closed")
+      }
+
+
+      val rePrompt = io.StdIn.readLine(s"Would you like to publish another message to topic '${topic}'? (y to continue, other to exit) > ")
+      rePrompt match {
+        case "y" | "Y" => messageLoop()
+        case _ =>
+          println()
+      }
     }
 
-
-    val rePrompt = io.StdIn.readLine("Would you like to publish another message? (y to continue, other to exit) > ")
+    val rePrompt = io.StdIn.readLine("Would you like to publish to another topic? (y to continue, other to exit) > ")
     rePrompt match {
-      case "y" | "Y" => promptLoop()
+      case "y" | "Y" => topicLoop()
       case _ =>
         println("Thank you, come again!")
     }
+
   }
 
-  promptLoop()
+
+  topicLoop()
   system.terminate()
 }
