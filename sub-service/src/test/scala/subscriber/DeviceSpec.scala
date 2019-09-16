a983a4322eb5aeef63bc22f2b35c16e0ef9085b6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-16 12:15:07 -0700

    Add basic device with Read/Response Messages

--- sub-service/src/test/scala/subscriber/DeviceSpec.scala
+++ sub-service/src/test/scala/subscriber/DeviceSpec.scala
@@ -1,13 +1,29 @@
 package subscriber
 
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 
 class DeviceSpec extends TestKit(ActorSystem("DeviceSpec"))
   with ImplicitSender
   with WordSpecLike
-  with BeforeAndAfterAll {
+  with BeforeAndAfterAll
+  with Matchers {
 
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A device actor" should {
+    "should response with None if no data is available" in {
+      val deviceActor = system.actorOf(Device.props("0001", "0002"))
+
+      deviceActor ! Device.ReadRef(1)
+      val response = expectMsgType[Device.RespondRef]
+      response.requestId should ===(1L)
+      response.value should ===(None)
+
+    }
+  }
 }
