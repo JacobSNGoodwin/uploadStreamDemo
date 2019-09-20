7ca62d61ea161f152342baf8cba9ce52b1929c9b
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-20 11:34:14 -0700

    Scaffold out a DeviceGroup actor

---
+++ sub-service/src/main/scala/subscriber/DeviceGroup.scala
@@ -0,0 +1,10 @@
+package subscriber
+
+import akka.actor.{Actor, ActorLogging, Props}
+
+object DeviceGroup {
+  def props(groupId: String): Props = Props(new DeviceGroup(groupId))
+}
+class DeviceGroup(groupId: String) extends Actor with ActorLogging {
+  override def receive: Receive = ???
+}
