fd8896d07625996262cceb7a7f175345990668c4
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-24 15:38:09 -0700

    Add group registration DeviceManager

--- sub-service/src/main/scala/subscriber/DeviceManager.scala
+++ sub-service/src/main/scala/subscriber/DeviceManager.scala
@@ -2,7 +2,7 @@ package subscriber
 
 import java.util.Base64
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 import akka.stream.alpakka.googlecloud.pubsub.ReceivedMessage
 import spray.json.{DefaultJsonProtocol, JsonParser}
 
@@ -23,13 +23,27 @@ object DeviceManager {
 
   // Messages for Device management
   final case class RequestTrackDevice(groupId: String, deviceId: String)
+  final case class RequestGroupList(requestId: Long)
+  final case class ReplyGroupList(requestId: Long, ids: Set[String])
+
+  final case class RequestActorRef(requestId: Long, groupId: String)
+  final case class ReplyActorRef(requestId: Long, groupRef: ActorRef)
+
   case object DeviceRegistered
 
 }
 class DeviceManager(ackWith: Any) extends Actor with ActorLogging {
   import DeviceManager._
   import CustomJsonProtocol._ // to provide implicits
-  override def receive: Receive = {
+
+  override def preStart(): Unit = log.info("Device Manager started")
+
+  override def postStop(): Unit = log.info("Device Manager stopped")
+
+  override def receive: Receive = managerReceiver(Map(), Map())
+
+  def managerReceiver(groupIdToActor: Map[String, ActorRef], actorToGroupId: Map[ActorRef, String]): Receive = {
+    // handle incoming messages from GC PubSub
     case StreamInitialized =>
       log.info("Stream initialized!")
       sender() ! Ack // ack to allow the stream to proceed sending more elements
@@ -41,5 +55,31 @@ class DeviceManager(ackWith: Any) extends Actor with ActorLogging {
       log.info("Stream completed!")
     case StreamFailure(ex) =>
       log.error(ex, "Stream failed!")
+
+    // handle device registration
+    case trackMsg @ RequestTrackDevice(groupId, _) =>
+      groupIdToActor.get(groupId) match {
+        case Some(ref) =>
+          ref.forward(trackMsg)
+        case None =>
+          log.info("Creating device group actor for {}", groupId)
+          val groupActor = context.actorOf(DeviceGroup.props(groupId), "group-" + groupId)
+          context.watch(groupActor)
+          groupActor.forward(trackMsg)
+          val newGroupIdToActor = groupIdToActor + (groupId -> groupActor)
+          val newActorToGroupId = actorToGroupId + (groupActor -> groupId)
+          context.become(managerReceiver(newGroupIdToActor, newActorToGroupId))
+      }
+
+    case RequestGroupList(requestId) =>
+      sender() ! ReplyGroupList(requestId, groupIdToActor.keySet)
+    case RequestActorRef(requestId, groupId) =>
+      sender() ! ReplyActorRef(requestId, groupIdToActor(groupId))
+    case Terminated(groupActor) =>
+        val groupId = actorToGroupId(groupActor)
+        log.info("Device group actor for {} has been terminated", groupId)
+        val newGroupIdToActor = groupIdToActor - groupId
+        val newActorToGroupId = actorToGroupId - groupActor
+        context.become(managerReceiver(newGroupIdToActor, newActorToGroupId))
   }
 }
---
+++ sub-service/src/test/scala/subscriber/DeviceManagerSpec.scala
@@ -0,0 +1,92 @@
+package subscriber
+
+import akka.actor.{ActorSystem, PoisonPill}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class DeviceManagerSpec extends TestKit(ActorSystem("DeviceGroupSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll
+  with Matchers {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A device manager" should {
+    "be able to register a device group" in {
+      val probe = TestProbe()
+      val managerActor = system.actorOf(DeviceManager.props(DeviceManager.Ack))
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor1 = probe.lastSender
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor2 = probe.lastSender
+      deviceActor1 should !==(deviceActor2)
+    }
+
+    "return same actor for same deviceId" in {
+      val probe = TestProbe()
+      val managerActor = system.actorOf(DeviceManager.props(DeviceManager.Ack))
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor1 = probe.lastSender
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor2 = probe.lastSender
+
+      deviceActor1 should ===(deviceActor2)
+    }
+
+    "be able to list active groups" in {
+      val probe = TestProbe()
+      val managerActor = system.actorOf(DeviceManager.props(DeviceManager.Ack))
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group1", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group2", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+
+      managerActor.tell(DeviceManager.RequestGroupList(requestId = 0), probe.ref)
+      probe.expectMsg(DeviceManager.ReplyGroupList(requestId = 0, Set("group1", "group2")))
+    }
+
+    "be able to list active groups after one shuts down" in {
+      val probe = TestProbe()
+      val managerActor = system.actorOf(DeviceManager.props(DeviceManager.Ack), "deviceManager")
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group1", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+
+      managerActor.tell(DeviceManager.RequestTrackDevice("group2", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+
+      managerActor.tell(DeviceManager.RequestGroupList(requestId = 0), probe.ref)
+      probe.expectMsg(DeviceManager.ReplyGroupList(requestId = 0, Set("group1", "group2")))
+
+      managerActor.tell(DeviceManager.RequestActorRef(1L, "group1"), probe.ref)
+      val response = probe.expectMsgType[DeviceManager.ReplyActorRef]
+
+      val toShutDown = response.groupRef
+
+      probe.watch(toShutDown)
+      toShutDown ! PoisonPill
+      probe.expectTerminated(toShutDown)
+
+      // using awaitAssert to retry because it might take longer for the groupActor
+      // to see the Terminated, that order is undefined
+      probe.awaitAssert {
+        managerActor.tell(DeviceManager.RequestGroupList(requestId = 1), probe.ref)
+        probe.expectMsg(DeviceManager.ReplyGroupList(requestId = 1, Set("group2")))
+      }
+    }
+  }
+
+}
