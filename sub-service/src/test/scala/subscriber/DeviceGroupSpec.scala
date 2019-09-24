e7aaa5076ebef9eddbe873fea56f301a59344c17
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-23 17:35:09 -0700

    Add device group registration tests

--- sub-service/src/test/scala/subscriber/DeviceGroupSpec.scala
+++ sub-service/src/test/scala/subscriber/DeviceGroupSpec.scala
@@ -3,6 +3,7 @@ package subscriber
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import scala.concurrent.duration._
 
 class DeviceGroupSpec extends TestKit(ActorSystem("DeviceGroupSpec"))
   with ImplicitSender
@@ -21,6 +22,41 @@ class DeviceGroupSpec extends TestKit(ActorSystem("DeviceGroupSpec"))
 
       groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
       probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor1 = probe.lastSender
+
+      groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor2 = probe.lastSender
+      deviceActor1 should !==(deviceActor2)
+
+      // check devices are responding to messages
+      deviceActor1.tell(Device.ReadFiles(1), probe.ref)
+      probe.expectMsg(Device.ReadFilesResponse(1L, None))
+      deviceActor1.tell(Device.ReadFiles(1), probe.ref)
+      probe.expectMsg(Device.ReadFilesResponse(1L, None))
+    }
+
+    "ignore requests for wrong groupId" in {
+      val probe = TestProbe()
+      val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+      groupActor.tell(DeviceManager.RequestTrackDevice("wrongGroup", "device1"), probe.ref)
+      probe.expectNoMessage(500 milliseconds)
+    }
+
+    "return same actor for same deviceId" in {
+      val probe = TestProbe()
+      val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+      groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor1 = probe.lastSender
+
+      groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      val deviceActor2 = probe.lastSender
+
+      deviceActor1 should ===(deviceActor2)
     }
   }
 
