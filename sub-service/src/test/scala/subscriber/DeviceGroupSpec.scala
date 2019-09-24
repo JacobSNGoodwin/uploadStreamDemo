20781a8ed5c3882c79774208dc087e3ea36cb9c6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-23 17:12:30 -0700

    Scaffold test for group actor

--- sub-service/src/test/scala/subscriber/DeviceGroupSpec.scala
+++ sub-service/src/test/scala/subscriber/DeviceGroupSpec.scala
@@ -1,5 +1,27 @@
 package subscriber
 
-class DeviceGroupSpec {
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class DeviceGroupSpec extends TestKit(ActorSystem("DeviceGroupSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll
+  with Matchers {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A device group" should {
+    "be able to register a device actor" in {
+      val probe = TestProbe()
+      val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+      groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+    }
+  }
 
 }
