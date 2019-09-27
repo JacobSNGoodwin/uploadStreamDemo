cad32ae335812e0f2624e2db932e6506f4655f03
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-09-26 22:03:33 -0700

    Establish connection to cloud storage

--- sub-service/src/main/scala/subscriber/FileActor.scala
+++ sub-service/src/main/scala/subscriber/FileActor.scala
@@ -4,12 +4,16 @@ import java.nio.file.Paths
 
 import akka.{Done, NotUsed}
 import akka.actor.{Actor, ActorLogging, ActorRef}
-import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.ContentTypes
+import akka.stream.{ActorMaterializer, IOResult}
 import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.ByteString
 import akka.pattern.pipe
+import akka.stream.alpakka.googlecloud.storage.{GCStorageAttributes, GCStorageExt, GCStorageSettings, StorageObject}
+import akka.stream.alpakka.googlecloud.storage.scaladsl.GCStorage
+import com.typesafe.config._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Random, Success}
 
 // Actor for handling async file reading and writing
@@ -17,9 +21,21 @@ object FileActor {
   case class Write(requestId: Long, filePath: String, originalRequester: ActorRef)
   case class FileRecorded(requestId: Long, filePath: String, originalRequester: ActorRef)
   case class FileRecordedFailed(originalRequester: ActorRef)
+
+  case class FileUpload(requestId: Long, filePath: String)
+  case class FileUploadResponse(requestId: Long, storageId: String)
+  case object FileUploadError
 }
 class FileActor extends Actor with ActorLogging {
   import FileActor._
+  val conf: Config = ConfigFactory.load()
+  val bucketName = conf.getString("gcConfig.bucket-name")
+  val ChunkSize = 256 * 1024
+
+  // deal with nasty newline issue when loading environment variables
+  val gcKey = conf.getString("gcConfig.key").replace("\\n", "\n")
+  val newPrivateKeySettings: GCStorageSettings = GCStorageExt(context.system).settings.withPrivateKey(gcKey)
+
 
   implicit val executionContext: ExecutionContext = context.dispatcher
   // materializer for akka streams API (file IO)
@@ -33,7 +49,7 @@ class FileActor extends Actor with ActorLogging {
     case Write(requestId: Long, filePath: String, originalRequester: ActorRef) =>
       log.info("Writing file for requestId: {} to filePath: {}", requestId, filePath)
       val file = Paths.get(filePath)
-      val text: Source[String, NotUsed] = Source(1 to 10).map(_ => Random.alphanumeric.take(100 * 1024).mkString) // source of 1kB chunks
+      val text: Source[String, NotUsed] = Source(1 to 100).map(_ => Random.alphanumeric.take(10 * 1024).mkString) // source of 1kB chunks
       // Future[IOResult] - map to a Device message which is piped to Device on completion
       text.map(t => ByteString(t)).runWith(FileIO.toPath(file))
         .map(iOResult => {
@@ -44,5 +60,17 @@ class FileActor extends Actor with ActorLogging {
               FileRecordedFailed(originalRequester)
           }
         }).pipeTo(sender())
+    case FileUpload(requestId, filePath) =>
+      log.info("Attempting file upload to bucket '{}' for the following file path: '{}'", bucketName, filePath)
+      val file = Paths.get(filePath)
+      val byteStringSource = FileIO.fromPath(file, ChunkSize)
+      val sink =
+        GCStorage
+          .resumableUpload(bucketName, file.getFileName.toString, ContentTypes.`text/plain(UTF-8)`, ChunkSize)
+          .withAttributes(GCStorageAttributes.settings(newPrivateKeySettings))
+      val result: Future[StorageObject] = byteStringSource.runWith(sink)
+      result.map(storageObject => {
+        FileUploadResponse(requestId, storageObject.id)
+      }).pipeTo(sender())
   }
 }
--- sub-service/src/test/scala/subscriber/FileActorSpec.scala
+++ sub-service/src/test/scala/subscriber/FileActorSpec.scala
@@ -26,6 +26,22 @@ class FileActorSpec extends TestKit(ActorSystem("FileActorSpec"))
       fileActor.tell(Write(54321L, "./file-storage/test-file.txt", fileActor), probe.ref)
       probe.expectMsg(FileRecorded(54321L, "./file-storage/test-file.txt", fileActor))
     }
+
+    "return a response on file upload" in {
+      val probe = TestProbe()
+      val fileActor = system.actorOf(Props[FileActor])
+      val filePath = "./file-storage/test-file.txt"
+
+      within(3.seconds){
+        fileActor.tell(Write(54321L, filePath, fileActor), probe.ref)
+        probe.expectMsg(FileRecorded(54321L, filePath, fileActor))
+      }
+
+      within(15.second){
+        fileActor.tell(FileUpload(12345L, filePath), probe.ref)
+        probe.expectMsgType[FileUploadResponse]
+      }
+    }
   }
 
 }
